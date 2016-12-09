276913a886fd31170bfdddc3bbafc84e607a1ab2
Yang Jiang <jiangyang5157@gmail.com>
2016-12-09 19:13:26 +1300

    ExternalStorage Writable and Readable

--- android-base/src/main/java/com/gmail/jiangyang5157/tookit/android/base/DeviceUtils.java
+++ android-base/src/main/java/com/gmail/jiangyang5157/tookit/android/base/DeviceUtils.java
@@ -75,8 +75,23 @@ public class DeviceUtils {
         return (int) px;
     }
 
-    public static boolean hasSdcard() {
-        return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)
+                || Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
     }
 
     public static File getSdacrdFile() {
