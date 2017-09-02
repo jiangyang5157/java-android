0a42e2747ebd44a4e4a2b4a8f62ee200a97c5216
yangjiang <jiangyang5157@gmail.com>
2017-09-02 14:47:42 +1200

    rename java-math pkg

--- java-math/src/main/java/com/gmail/jiangyang5157/java_math/Geometry.java
+++ java-math/src/main/java/com/gmail/jiangyang5157/java_math/Geometry.java
@@ -1,4 +1,4 @@
-package com.gmail.jiangyang5157.tookit.math;
+package com.gmail.jiangyang5157.java_math;
 
 /**
  * @author Yang 4/9/2016.
--- java-math/src/main/java/com/gmail/jiangyang5157/java_math/Mersenne.java
+++ java-math/src/main/java/com/gmail/jiangyang5157/java_math/Mersenne.java
@@ -1,4 +1,4 @@
-package com.gmail.jiangyang5157.tookit.math;
+package com.gmail.jiangyang5157.java_math;
 
 import java.util.Random;
 
