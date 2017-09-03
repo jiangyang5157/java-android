b99d38619d6f6f288b7bb64caecb161cf6c6ab3b
yangjiang <jiangyang5157@gmail.com>
2017-09-03 16:06:54 +1200

    na

---
+++ java-rest/src/main/java/com/gmail/jiangyang5157/java_rest/http/Rest.java
@@ -0,0 +1,37 @@
+package com.gmail.jiangyang5157.java_rest.http;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Yang Jiang on September 03, 2017
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+public @interface Rest {
+
+    /**
+     * @return the root url of the web service
+     */
+    String rootUrl();
+
+    /**
+     * @return the converter class to convert received data into Java object
+     */
+    Class<?> converter();
+
+    /**
+     * @return the interceptor class to do extra processing before or after request
+     */
+    Class<?> interceptor();
+
+    /**
+     * @return the error handler class to handle errors
+     */
+    Class<?> errorHandler();
+}
