baa6deaf339049fe93e6d02b33ed34c574722582
yangjiang <jiangyang5157@gmail.com>
2017-09-03 16:23:54 +1200

    testImplementation junit

---
+++ java-rest/src/test/java/com/gmail/jiangyang5157/java_rest/RestServiceTest.java
@@ -0,0 +1,17 @@
+package com.gmail.jiangyang5157.java_rest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Yang Jiang on September 03, 2017
+ */
+
+public class RestServiceTest {
+
+    @Test
+    public void test1() {
+        assertEquals(1, 1);
+    }
+}
