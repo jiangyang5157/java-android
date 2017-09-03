0c5c6c322b075829fa0dbe8297c14e645b6ef666
yangjiang <jiangyang5157@gmail.com>
2017-09-03 15:42:10 +1200

    na

--- java-rest/src/main/java/com/gmail/jiangyang5157/java_rest/error/RestErrorHandler.java
+++
@@ -1,9 +0,0 @@
-package com.gmail.jiangyang5157.java_rest.error;
-
-/**
- * Created by Yang Jiang on September 03, 2017
- */
-
-public interface RestErrorHandler {
-    void onRestExceptionThrown(RestException e);
-}
