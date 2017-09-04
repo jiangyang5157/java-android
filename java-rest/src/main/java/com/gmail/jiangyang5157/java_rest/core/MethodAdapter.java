849137dea48565b30765321c4d8a5c670517e456
yangjiang <jiangyang5157@gmail.com>
2017-09-05 01:40:10 +1200

    na

---
+++ java-rest/src/main/java/com/gmail/jiangyang5157/java_rest/core/MethodAdapter.java
@@ -0,0 +1,12 @@
+package com.gmail.jiangyang5157.java_rest.core;
+
+/**
+ * Created by Yang Jiang on September 05, 2017
+ */
+
+/**
+ * Adapts an invocation of an interface method into an HTTP call.
+ */
+final class MethodAdapter {
+    // TODO: 5/09/17
+}
